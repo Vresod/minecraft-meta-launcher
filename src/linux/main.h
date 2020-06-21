12b9f4a2261efc4fce7a72cb58b8ba779900f459
Vresod <lukebinkofsky@gmail.com>
2020-06-20 20:21:57 -0400

    removed minecraft-bedrock launcher from linux this is replaced by MCPE.AppImage

--- src/linux/main.h
+++ src/linux/main.h
@@ -9,21 +9,16 @@
 
 
 void MCPEApp(){
-    // curl the file
-    system("curl -L https://github.com/ChristopherHX/linux-packaging-scripts/releases/download/appimage/Minecraft_Bedrock_Launcher-x86_64.0.0.510.AppImage --output MCPE.AppImage");
-    // run the file
-    system("MCPE.AppImage");
+	// curl the file
+	system("curl -L https://github.com/ChristopherHX/linux-packaging-scripts/releases/download/appimage/Minecraft_Bedrock_Launcher-x86_64.0.0.510.AppImage --output MCPE.AppImage");
+	// run the file
+	system("MCPE.AppImage");
 }
 
 void multimc(){
-    system("multimc");
+	system("multimc");
 }
 
 void minecraft_launcher(){
-    system("minecraft-launcher");
-
-}
-
-void mcBedrock(){
-
+	system("minecraft-launcher");
 }
