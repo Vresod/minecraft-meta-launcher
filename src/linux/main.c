e82e878cdcdd2233e546b65910980e62de541a07
Vresod <lukebinkofsky@gmail.com>
2020-06-20 21:34:06 -0400

    added some little choice code currently segfaults also added .gitignore

--- src/linux/main.c
+++ src/linux/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+int choice;
 
 int main(int argc, char* argv[]){
-
+	printf("1. Minecraft Launcher\n");
+	printf("2. MultiMC5\n");
+	printf("3. mcpelauncher\n");
+	printf("> ");
+	scanf("%d",choice);
+	printf("You have chosen %d.",choice);
 }
