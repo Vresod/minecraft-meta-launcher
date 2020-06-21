8f4e20adcd88640829b6404112a10b6738f68977
Vresod <lukebinkofsky@gmail.com>
2020-06-20 21:53:07 -0400

    added launching this is currently nonfunctional

--- src/linux/main.c
+++ src/linux/main.c
@@ -9,5 +9,18 @@ int main(int argc, char* argv[]){
 	printf("3. mcpelauncher\n");
 	printf("> ");
 	scanf ("%s",choice);
-	printf("You have chosen %s",choice);
+	switch((int)choice) {
+		case 1:
+			printf("Opening minecraft-launcher...");
+			minecraft_launcher();
+			break;
+		case 2:
+			printf("Opening MultiMC5...");
+			multimc();
+			break;
+		case 3:
+			printf("Opening mcpelauncher...");
+			MCPEApp();
+			break;
+	}
 }
