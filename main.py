95e56e92a0bcb4e65efffdaad872b2d12432d1b4
Vresod <lukebinkofsky@gmail.com>
2020-06-21 13:50:53 -0400

    now uses requests instead of subprocess to download file

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 import subprocess
+import os
 import extra
+import requests
+from pathlib import Path
+home = str(Path.home())
+
 
 print("1. Minecraft Launcher")
 print("2. MultiMC")
@@ -22,6 +27,7 @@ elif(launcher_choice == "3"):
 		download_choice = input("Would you like to update MCPE.AppImage? (Y/n) > ")
 	if(download_choice == "Y"):
 		print("Downloading MCPE.AppImage...")
-		subprocess.run(["curl", "-L", "https://github.com/ChristopherHX/linux-packaging-scripts/releases/download/appimage/Minecraft_Bedrock_Launcher-x86_64.0.0.510.AppImage", "--output", "MCPE.AppImage"])
+		appimage = requests.get("https://github.com/ChristopherHX/linux-packaging-scripts/releases/download/appimage/Minecraft_Bedrock_Launcher-x86_64.0.0.510.AppImage", allow_redirects=True)
+		open("MCPE.AppImage", 'wb').write(appimage.content)
 	subprocess.run(["chmod", "+x", "MCPE.AppImage"])
 	subprocess.run(["./MCPE.AppImage"])
