2b692ea8cf23e3205ac53a9250a527b403ee124a
AVCADO <devavcado@gmail.com>
2020-06-20 19:14:37 -0500

    added some things

--- src/linux/main.h
+++ src/linux/main.h
@@ -1,1 +1,27 @@
-#pragma once
+// headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// functions
+
+// launch functions
+
+
+
+void MCPEApp(){
+    // curl the file
+    system("curl -L https://github.com/ChristopherHX/linux-packaging-scripts/releases/download/appimage/Minecraft_Bedrock_Launcher-x86_64.0.0.510.AppImage --output MCPE.AppImage");
+    // run the file
+    system("MCPE.AppImage");
+}
+
+void multimc(){
+}
+
+void minecraft_launcher(){
+
+}
+
+void mcBedrock(){
+
+}
